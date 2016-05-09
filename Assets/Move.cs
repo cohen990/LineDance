d3a4498ec67dea56df7d25a06d8ab7aa6a071698
Dan Cohen <dan.cohen@euromoneyplc.com>
2016-05-09 21:44:33 +0100

    added a line

---
+++ Assets/Move.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach(var touch in Input.touches) {
+			GetComponent<Rigidbody2D> ().rotation += 5;
+		}
+	}
+}
