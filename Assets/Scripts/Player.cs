9bd9ead039c1d5a29613fc50ed893f8555de01f7
Dan Cohen <dan.cohen@euromoneyplc.com>
2016-07-21 23:14:02 +0100

    working on missed collisions

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using AssemblyCSharp;
 using AssemblyCSharp.Movement;
 using System.Diagnostics;
 using UnityEngine.SceneManagement;
+using Debug = UnityEngine.Debug;
 
 public class Player : MonoBehaviour {
 	private ControlBase _controls;
@@ -18,6 +19,7 @@ public class Player : MonoBehaviour {
 	private Vector3 _previousCarrierPosition;
 	private bool _isEndOfLevel;
 	internal INodeController ConnectedNodeController;
+	private Vector3[] _previousFramePointsAlongLength;
 
 	public Player(){
 	}
@@ -43,11 +45,11 @@ public class Player : MonoBehaviour {
 		catch(Exception){
 			UnityEngine.Debug.Log("No overlord was found. Hopefully you are testing ;)");
 		}
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//_movement.Check
 		if (_isDead) {
 			_overlord.PlayerDie ();
 		}
@@ -64,6 +66,14 @@ public class Player : MonoBehaviour {
 		if (_isBeingCarried) {
 			DoCarryMotion ();
 		}
+
+		var currentFramePointsAlongLength = GetPointsAlongLength ();
+
+		if (CheckForMissedCollisions (currentFramePointsAlongLength)) {
+			Debug.Log ("missed collision");
+		}
+
+		_previousFramePointsAlongLength = currentFramePointsAlongLength;
 	}
 
 	void OnTriggerEnter2D(Collider2D col){
@@ -161,6 +171,77 @@ public class Player : MonoBehaviour {
 		if (Application.platform == RuntimePlatform.Android) {
 			return true;
 		}
+		return false;
+	}
+
+	Vector3[] GetPointsAlongLength(){
+		var centre = gameObject.transform.position;
+		var collider = gameObject.GetComponent<BoxCollider2D> ();
+		var size = collider.bounds;
+		var rotation = gameObject.transform.eulerAngles;
+
+		var firstPointX = size.center.x + size.extents.x;
+		var firstPointY = size.center.y + size.extents.y;
+		var lastPointX = size.center.x - size.extents.x;
+		var lastPointY = size.center.y - size.extents.y;
+
+		if (rotation.z < 90 || rotation.z < 270) {
+			firstPointX = size.center.x - size.extents.x;
+			lastPointX = size.center.x + size.extents.x;
+		}
+		if (rotation.z >= 90 && rotation.z < 180) {
+			firstPointY = size.center.y - size.extents.y;
+			lastPointY = size.center.y + size.extents.y;
+		}
+
+		var firstPoint = new Vector3 (firstPointX, firstPointY);
+
+		var lastPoint = new Vector3 (lastPointX, lastPointY);
+
+		var points = GetPointsBetweenPoints (firstPoint, lastPoint);
+
+		return points;
+	}
+
+	Vector3[] GetPointsBetweenPoints(Vector3 beginning, Vector3 end, int numberOfPoints = 20){
+		var arr = new Vector3[numberOfPoints + 1];
+		arr [0] = beginning;
+		arr [numberOfPoints] = end;
+
+		var xDistance = beginning.x - end.x;
+		var yDistance = beginning.y - end.y;
+		var xInterval = xDistance / numberOfPoints;
+		var yInterval = yDistance / numberOfPoints;
+
+		for(var i = 1; i < numberOfPoints;i++){
+			arr [i] = beginning - new Vector3 (xInterval * i, yInterval * i);
+		}
+
+		return arr;
+	}
+
+	bool CheckForMissedCollisions (Vector3[] points)
+	{
+		if (_previousFramePointsAlongLength == null || points == null) {
+			return false;
+		}
+
+		for (var i = 0; i < points.Length; i++) {
+			Debug.DrawLine(points[i], _previousFramePointsAlongLength[i]);
+			var hit = Physics2D.Raycast (points [i], _previousFramePointsAlongLength [i]);
+			if (hit.collider != null) {
+				if (hit.collider.gameObject.tag.ToLower() == "node" && hit.collider.gameObject.transform.position != ConnectedNodeController.NodePosition) {
+					var node = hit.collider.gameObject;
+
+					var controller = node.GetComponent<INodeController> ();
+
+					if (controller.IsEnabled) {
+						return true;
+					}
+				}
+			}
+		}
+
 		return false;
 	}
 }
